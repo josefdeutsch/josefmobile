5a86b0db2afb4385d800e9e1b074e3b7f9b54041
philippdeutsch <philippdeutsch@icloud.com>
2020-04-03 15:56:57 -0700

    success design recyclerview, viewpager- collapsing
    Signed-off-by: philippdeutsch <philippdeutsch@icloud.com>

--- app/src/main/java/com/josef/mobile/MainActivity.java
+++ app/src/main/java/com/josef/mobile/MainActivity.java
@@ -2,37 +2,29 @@ package com.josef.mobile;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.ViewCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager.widget.ViewPager;
-import androidx.viewpager2.widget.MarginPageTransformer;
 import androidx.viewpager2.widget.ViewPager2;
-
-import android.content.res.Resources;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.TypedValue;
 import android.view.View;
 
 import com.josef.josefmobile.R;
 import com.josef.mobile.components.MainActivityAdapter;
 import com.josef.mobile.components.MainActivityViewPagerAdapter;
-import com.josef.mobile.components.MyAdapter;
 import com.josef.mobile.model.MainActivityAdapterBody;
 
 import java.util.ArrayList;
 
-import static androidx.viewpager2.widget.ViewPager2.ORIENTATION_HORIZONTAL;
 
-public class MainActivity extends AppCompatActivity implements MainActivityViewPagerFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity {
 
     private MainActivityViewPagerAdapter mAdapter;
     private ViewPager2 mViewPager2;
     ViewPager2 myViewPager2;
-    private MyAdapter myAdapter;
+    private MainActivityViewPagerAdapter myAdapter;
     private ArrayList<String> arrayList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,20 +33,14 @@ public class MainActivity extends AppCompatActivity implements MainActivityViewP
         setContentView(R.layout.activity_main);
         setupRecyclerView();
 
-        //setupAdapter();
-        //registerListener();
-
-
-            // Initializing view pager
         myViewPager2 = findViewById(R.id.viewPager2);
-
-        myAdapter = new MyAdapter(this);
+        myAdapter = new MainActivityViewPagerAdapter(this);
         myViewPager2.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
         myViewPager2.setAdapter(myAdapter);
         myViewPager2.setOffscreenPageLimit(3);
 
-       final float pageMargin= getResources().getDimensionPixelOffset(R.dimen.pageMargin);
-       final float pageOffset = getResources().getDimensionPixelOffset(R.dimen.offset);
+        final float pageMargin = getResources().getDimensionPixelOffset(R.dimen.pageMargin);
+        final float pageOffset = getResources().getDimensionPixelOffset(R.dimen.offset);
 
         myViewPager2.setPageTransformer(new ViewPager2.PageTransformer() {
             @Override
@@ -71,68 +57,14 @@ public class MainActivity extends AppCompatActivity implements MainActivityViewP
                 }
             }
         });
-        /**myViewPager2.setPageTransformer(new ViewPager2.PageTransformer() {
-            @Override
-            public void transformPage(@NonNull View page, float position) {
-                float myOffset = position * -(2 * pageOffset + pageMargin);
-                if (position < -1) {
-                    page.setTranslationX(-myOffset);
-                } else if (position <= 1) {
-                    float scaleFactor = Math.max(0.7f, 1 - Math.abs(position - 0.14285715f));
-                    page.setTranslationX(myOffset);
-                    page.setScaleY(scaleFactor);
-                    page.setAlpha(scaleFactor);
-                } else {
-                    page.setAlpha(0);
-                    page.setTranslationX(myOffset);
-                }
-            }
-        });
-**/
 
-        /**myViewPager2.setPageTransformer(new ViewPager2.PageTransformer() {
-            @Override
-            public void transformPage(@NonNull View page, float position) {
-                float myOffset = position * -(2 * pageOffset + pageMargin);
-                if (position < -1) {
-                    page.setTranslationX(-myOffset);
-                } else if (position <= 1) {
-                    float scaleFactor = Math.max(0.7f, 1 - Math.abs(position - 0.14285715f));
-                    page.setTranslationX(myOffset);
-                    page.setScaleY(scaleFactor);
-                    page.setAlpha(scaleFactor);
-                } else {
-                    page.setAlpha(0);
-                    page.setTranslationX(myOffset);
-                }
-            }
-        });**/
-
-
-    }
-
-
-
-    private void registerListener() {
-        mViewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
-            }
-        });
-    }
-
-    private void setupAdapter() {
-        mViewPager2 = findViewById(R.id.viewPager2);
-        mAdapter = new MainActivityViewPagerAdapter(getSupportFragmentManager(),getLifecycle());
-        mViewPager2.setOrientation(ORIENTATION_HORIZONTAL);
-        mViewPager2.setAdapter(mAdapter);
+        myViewPager2.setCurrentItem(1);
     }
 
 
     private void setupRecyclerView() {
         RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        GridLayoutManager layoutManager = new GridLayoutManager(this,2);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
         mRecyclerView.setLayoutManager(layoutManager);
         mRecyclerView.setHasFixedSize(true);
         final MainActivityAdapter simpleAdapter = new MainActivityAdapter(getList(new ArrayList()));
@@ -140,17 +72,11 @@ public class MainActivity extends AppCompatActivity implements MainActivityViewP
     }
 
 
-
-
-    private ArrayList<MainActivityAdapterBody> getList(ArrayList<MainActivityAdapterBody> arrayList){
-        for (int i = 0; i <= 10 -1 ; i++) {
-            arrayList.add(new MainActivityAdapterBody("name :"+i,"description :"+i));
+    private ArrayList<MainActivityAdapterBody> getList(ArrayList<MainActivityAdapterBody> arrayList) {
+        for (int i = 0; i <= 10 - 1; i++) {
+            arrayList.add(new MainActivityAdapterBody("name :" + i, "description :" + i));
         }
         return arrayList;
     }
 
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
-    }
 }
--- app/src/main/java/com/josef/mobile/components/MainActivityViewPagerAdapter.java
+++ app/src/main/java/com/josef/mobile/components/MainActivityViewPagerAdapter.java
@@ -1,39 +1,44 @@
 package com.josef.mobile.components;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.lifecycle.Lifecycle;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+import com.josef.josefmobile.R;
 
-import com.josef.mobile.MainActivityViewPagerFragment;
+public class MainActivityViewPagerAdapter extends RecyclerView.Adapter<MainActivityViewPagerAdapter.MyViewHolder> {
 
+    private Context context;
 
-public class MainActivityViewPagerAdapter extends FragmentStateAdapter {
-
-
-    public MainActivityViewPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
-        super(fragmentManager, lifecycle);
-    }
-
-    @Override
-    public int getItemCount() {
-        return 3;
+    public MainActivityViewPagerAdapter(Context context) {
+        this.context = context;
     }
 
     @NonNull
     @Override
-    public Fragment createFragment(int position) {
-        switch (position) {
-            case 0:
-                return MainActivityViewPagerFragment.newInstance("fdfd", "dfdfdf");
-            case 1:
-                return MainActivityViewPagerFragment.newInstance("ddfaaadf", "dfdfdf");
-            case 2:
-                return MainActivityViewPagerFragment.newInstance("ddfd7676f", "dfdfdf");
-
-        }
-        return null;
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.mainactiviy_viewpagerfragment, parent, false);
+        return new MyViewHolder(view);
     }
 
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 15;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+        }
+    }
 }
