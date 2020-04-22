9b83740e3c8a6b3bd81a02751d5ac0d7b366f9e1
philippdeutsch <philippdeutsch@icloud.com>
2020-04-22 04:39:58 -0700

    success tdd badrequest
    Signed-off-by: philippdeutsch <philippdeutsch@icloud.com>

--- app/src/androidTest/java/com/josef/mobile/ErrorActivityTest.java
+++ app/src/androidTest/java/com/josef/mobile/ErrorActivityTest.java
@@ -1,13 +1,16 @@
 package com.josef.mobile;
 
+import android.content.Context;
+import android.content.Intent;
+
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.josef.josefmobile.R;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import java.io.IOException;
 
 import okhttp3.HttpUrl;
@@ -15,68 +18,52 @@ import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import okhttp3.mockwebserver.Dispatcher;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
-import okhttp3.mockwebserver.RecordedRequest;
 import static org.junit.Assert.*;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class ErrorActivityTest {
 
+    //https://riptutorial.com/android/example/12328/mockwebserver-example
+    //https://github.com/square/okhttp/tree/master/mockwebserver
+    //https://www.youtube.com/results?search_query=mockwebserver+android
+
+    private static final String TAG = "ErrorActivityTest";
     @Test
-public void hure(){
-    try (ActivityScenario<SplashActivity> scenario = ActivityScenario.launch(SplashActivity.class)) {
-        scenario.onActivity(new ActivityScenario.ActivityAction<SplashActivity>() {
-            @Override
-            public void perform(SplashActivity activity) {
-
-            }
-        });
-    }
-    MockWebServer webServer = new MockWebServer();
-    webServer.enqueue(new MockResponse().setResponseCode(404));
-    HttpUrl baseUrl = webServer.url("api/key");
-    try {
-//            webServer.start();
-        if(getResponseCode(new OkHttpClient(),baseUrl)>=300){
-
-        }
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-
-}
-    final Dispatcher dispatcher = new Dispatcher() {
-        @Override
-        public MockResponse dispatch (RecordedRequest request) throws InterruptedException {
-            return new MockResponse().setResponseCode(404);
-        }
-    };
-
-    private int getResponseCode(OkHttpClient client,HttpUrl base) throws IOException {
-        RequestBody body = RequestBody.create(MediaType.parse("text/plain"),"hi there");
-        okhttp3.Request request = new okhttp3.Request.Builder().post(body).url(base).build();
-        Response response = client.newCall(request).execute();
-        return response.code();
-    }
-
-
-    @Test
-    public void fdfgdfg() {
+    public void test_if_erroractivity_exsist() {
         ActivityScenario<ErrorActivity> scenario = ActivityScenario.launch(ErrorActivity.class);
         scenario.onActivity(new ActivityScenario.ActivityAction<ErrorActivity>() {
             @Override
             public void perform(ErrorActivity activity) {
-                //ErrorActivity
-                assertEquals("ErrorActivity","ErrorActivity");
+                assertEquals("ErrorActivity",activity.TAG);
             }
         });
+    }
+    @Test
+    public void test_if_erroractivity_is_init_badrequest() throws IOException {
 
+        MockWebServer webServer = new MockWebServer();
+        OkHttpClient client = new OkHttpClient();
+        webServer.enqueue(new MockResponse().setResponseCode(404));
+
+        HttpUrl base= webServer.url("api/key");
+        RequestBody body = RequestBody.create(MediaType.parse("text/plain"),"hi there");
+        final okhttp3.Request request = new okhttp3.Request.Builder().post(body).url(base).build();
+        Response response = client.newCall(request).execute();
+
+        if(response.code()>=300){
+            Context targetContext = InstrumentationRegistry.getInstrumentation()
+                    .getTargetContext();
+            Intent result = new Intent(targetContext, ErrorActivity.class);
+            ActivityScenario.launch(result);
+        }
+
+        webServer.shutdown();
+        onView(withId(R.id.errorView)).check(matches(isDisplayed()));
     }
 }
--- app/src/main/java/com/josef/mobile/ErrorActivity.java
+++ app/src/main/java/com/josef/mobile/ErrorActivity.java
@@ -1,16 +1,48 @@
 package com.josef.mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewTreeObserver;
 
+import com.google.android.material.bottomappbar.BottomAppBar;
 import com.josef.josefmobile.R;
 
 public class ErrorActivity extends AppCompatActivity {
     public static final String TAG = "ErrorActivity";
+    BottomAppBar bar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_error);
+        bar = (BottomAppBar) findViewById(R.id.bottom_app_bar);
+        setSupportActionBar(bar);
+        bar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the navigation click by showing a BottomDrawer etc.
+            }
+        });
+        bar.setFabAlignmentMode(BottomAppBar.FAB_ALIGNMENT_MODE_END);
+
+
+        final NestedScrollView scrollView = findViewById(R.id.nested_scrollview);
+        scrollView.getViewTreeObserver()
+                .addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+                    @Override
+                    public void onScrollChanged() {
+                        if (scrollView.getScrollY() == 0) {
+                            bar.performShow();
+                        } else if (scrollView.getChildAt(0).getBottom()
+                                <= (scrollView.getHeight() + scrollView.getScrollY())) {
+                            bar.performHide();
+                        } else {
+                            //scroll view is not at bottom
+                        }
+                    }
+                });
     }
 }
