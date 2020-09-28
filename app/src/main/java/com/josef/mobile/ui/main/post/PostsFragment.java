9874e0c36eb0adaf63086b6f1b2d5ea9468d6422
philippdeutsch <philippdeutsch@icloud.com>
2020-09-28 11:23:30 -0700

    Joseph2 Live Data Json to Recyler...
    Signed-off-by: philippdeutsch <philippdeutsch@icloud.com>

--- app/src/main/java/com/josef/mobile/ui/main/post/PostsFragment.java
+++ app/src/main/java/com/josef/mobile/ui/main/post/PostsFragment.java
@@ -14,7 +14,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.josef.mobile.R;
-import com.josef.mobile.models.Post;
 import com.josef.mobile.ui.main.Resource;
 import com.josef.mobile.util.VerticalSpaceItemDecoration;
 import com.josef.mobile.viewmodels.ViewModelProviderFactory;
@@ -22,6 +21,7 @@ import com.josef.mobile.viewmodels.ViewModelProviderFactory;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.json.JsonObject;
 
 import dagger.android.support.DaggerFragment;
 
@@ -55,9 +55,9 @@ public class PostsFragment extends DaggerFragment {
 
     private void subscribeObservers() {
         viewModel.observePosts().removeObservers(getViewLifecycleOwner());
-        viewModel.observePosts().observe(getViewLifecycleOwner(), new Observer<Resource<List<Post>>>() {
+        viewModel.observePosts().observe(getViewLifecycleOwner(), new Observer<Resource<List<JsonObject>>>() {
             @Override
-            public void onChanged(Resource<List<Post>> listResource) {
+            public void onChanged(Resource<List<JsonObject>> listResource) {
                 if (listResource != null) {
                     switch (listResource.status) {
                         case LOADING: {
@@ -67,7 +67,13 @@ public class PostsFragment extends DaggerFragment {
 
                         case SUCCESS: {
                             Log.d(TAG, "onChanged: PostsFragment: got posts.");
-                            adapter.setPosts(listResource.data);
+                            Log.d(TAG, "onChanged: " + listResource.data);
+                            List<JsonObject> list = listResource.data;
+
+                            for (JsonObject object : list) {
+                                Log.d(TAG, "onChanged: " + object.getString("text"));
+                            }
+                            //adapter.setPosts(listResource.data);
                             break;
                         }
 
