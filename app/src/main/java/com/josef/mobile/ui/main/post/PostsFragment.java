ef0fa966527972e4afcee918150eaded2f261c08
philippdeutsch <philippdeutsch@icloud.com>
2020-11-19 14:55:43 -0800

    Joseph2 Recyclerview addonScrollListener Fullscreen IMMERSIVE
    Signed-off-by: philippdeutsch <philippdeutsch@icloud.com>

--- app/src/main/java/com/josef/mobile/ui/main/post/PostsFragment.java
+++ app/src/main/java/com/josef/mobile/ui/main/post/PostsFragment.java
@@ -44,7 +44,7 @@ public class PostsFragment extends BaseFragment
     private PostsViewModel viewModel;
 
     private RecyclerView recyclerView;
-
+    boolean scroll_down = false;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -111,13 +111,48 @@ public class PostsFragment extends BaseFragment
     }
 
 
+
     private void initRecyclerView() {
         GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 2);
         recyclerView.setLayoutManager(gridLayoutManager);
-       // recyclerView.addItemDecoration(new ItemOffsetDecoration(0));
+        // recyclerView.addItemDecoration(new ItemOffsetDecoration(0));
         recyclerView.setAdapter(adapter);
         recyclerView.setHasFixedSize(true);
         recyclerView.setItemViewCacheSize(20);
+
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+
+                if (scroll_down) {
+                    getActivity().getWindow().getDecorView().setSystemUiVisibility(
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                                    | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                                    | View.SYSTEM_UI_FLAG_IMMERSIVE);
+                } else {
+                    getActivity().getWindow().getDecorView().setSystemUiVisibility(
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (dy > 70) {
+                    scroll_down = true;
+
+                } else if (dy < -5) {
+                    scroll_down = false;
+                }
+            }
+        });
         recyclerView.setDrawingCacheEnabled(true);
         recyclerView.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
     }
