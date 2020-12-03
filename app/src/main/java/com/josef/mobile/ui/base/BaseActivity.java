173126d9a3c3701f699cec5b8943df98e98e36c8
philippdeutsch <philippdeutsch@icloud.com>
2020-12-03 11:34:24 -0800

    Joseph2 WorkerManager Server Ping intercept2
    Signed-off-by: philippdeutsch <philippdeutsch@icloud.com>

--- app/src/main/java/com/josef/mobile/ui/base/BaseActivity.java
+++ app/src/main/java/com/josef/mobile/ui/base/BaseActivity.java
@@ -1,30 +1,33 @@
 package com.josef.mobile.ui.base;
 
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
-import androidx.lifecycle.Observer;
+import androidx.work.Constraints;
+import androidx.work.Data;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
 
+import com.google.firebase.database.annotations.NotNull;
 import com.josef.mobile.SessionManager;
 import com.josef.mobile.data.DataManager;
 import com.josef.mobile.ui.auth.AuthActivity;
-import com.josef.mobile.ui.auth.AuthResource;
-import com.josef.mobile.ui.auth.model.User;
-import com.josef.mobile.ui.main.ConnectionLiveData;
 import com.josef.mobile.utils.UtilManager;
+import com.josef.mobile.utils.work.CallBackWorker;
 
 import javax.inject.Inject;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
 import static com.josef.mobile.ui.auth.AuthActivity.RC_SIGN_OUT;
+import static com.josef.mobile.utils.work.Worker.WORKREQUEST_INDICATOR;
+import static com.josef.mobile.utils.work.Worker.WORKREQUEST_KEYTASK_ERROR;
 
 public abstract class BaseActivity extends DaggerAppCompatActivity {
-
-    private static final String TAG = "DaggerExample";
 
     Activity activity;
 
@@ -34,46 +37,83 @@ public abstract class BaseActivity extends DaggerAppCompatActivity {
     @Inject
     public DataManager dataManager;
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        subscribeToSessionManager();
-        activity = this;
-        ConnectionLiveData connectionLiveData = new ConnectionLiveData(this);
-        connectionLiveData.removeObservers(this);
-        connectionLiveData.observe(this, new Observer<Boolean>() {
-            @Override
-            public void onChanged(Boolean aBoolean) {
-
-                if (aBoolean) {
-                    utilManager.hideNoInternetConnection();
-                } else {
-                    utilManager.showNoInternetConnection(activity);
-                }
-            }
-        });
-    }
-
     @Inject
     public UtilManager utilManager;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        verifyNetworkResponse();
+        subscribeToSessionManager();
+    }
+
+    private void verifyNetworkResponse() {
+        String activity = getActivityName();
+        Constraints constraints = buildNetworkConstraints();
+        Data data = buildData(activity);
+        OneTimeWorkRequest onetimeJob = buildOneTimeRequest(constraints, data);
+        ping(onetimeJob);
+    }
+
+    @org.jetbrains.annotations.NotNull
+    private Constraints buildNetworkConstraints() {
+        return new Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build();
+    }
+
+    @org.jetbrains.annotations.NotNull
+    private Data buildData(String activity) {
+        return new Data.Builder()
+                .putString(WORKREQUEST_INDICATOR, activity)
+                .build();
+    }
+
+    @org.jetbrains.annotations.NotNull
+    private OneTimeWorkRequest buildOneTimeRequest(Constraints constraints, Data data) {
+        return new OneTimeWorkRequest.Builder(CallBackWorker.class)
+                .setConstraints(constraints)
+                .setInputData(data)
+                .build();
+    }
+
+    private void ping(OneTimeWorkRequest onetimeJob) {
+        WorkManager.getInstance(this).enqueue(onetimeJob);
+        WorkManager.getInstance(this).getWorkInfoByIdLiveData(onetimeJob.getId())
+                .observe(this, workInfo -> {
+                    if (workInfo != null) {
+                        if (workInfo.getState().isFinished()) {
+                            if (getCallBackResult(workInfo) != null) finish();
+                        }
+                    }
+                });
+    }
+
+    private String getActivityName() {
+        activity = this;
+        return activity.getComponentName().getClassName();
+    }
+
+    protected String getCallBackResult(@NotNull WorkInfo workInfo) throws IllegalArgumentException {
+        Data data = workInfo.getOutputData();
+        return data.getString(WORKREQUEST_KEYTASK_ERROR);
+    }
 
     public void subscribeToSessionManager() {
         if (sessionManager.getAuthUser() == null) return;
-        sessionManager.getAuthUser().observe(this, new Observer<AuthResource<User>>() {
-            @Override
-            public void onChanged(AuthResource<User> userAuthResource) {
-                if (userAuthResource != null) {
-                    switch (userAuthResource.status) {
-                        case NOT_AUTHENTICATED: {
-                            navLoginScreen();
-                            break;
-                        }
+        sessionManager.getAuthUser().observe(this, userAuthResource -> {
+            if (userAuthResource != null) {
+                switch (userAuthResource.status) {
+                    case NOT_AUTHENTICATED: {
+                        navLoginScreen();
+                        break;
                     }
                 }
             }
         });
     }
+
     private void navLoginScreen() {
         Intent intent = new Intent(this, AuthActivity.class);
         startActivityForResult(intent, RC_SIGN_OUT);
---
+++ app/src/main/java/com/josef/mobile/ui/err/ErrorActivity.java
@@ -0,0 +1,17 @@
+package com.josef.mobile.ui.err;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.josef.mobile.R;
+
+public class ErrorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_error);
+
+    }
+}
